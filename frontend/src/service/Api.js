984d9e0b867d464486d8e61e89ec75af64e47849
Shemo <moshe.frydmann@gmail.com>
2022-01-25 14:17:13 +0200

    ajout jsdoc v1

--- frontend/src/service/Api.js
+++ frontend/src/service/Api.js
@@ -1,3 +1,18 @@
+/* PERMET DE CHECKER LES TYPES */
+//@ts-check
+
+/**
+ * @file Api.js est le fichier qui contient les "services" de l'application.
+ * @author Moshé Frydmann
+ * @see <a href="https://github.com/ShemoMFR/p12_OC/tree/master" target='_blank'>Repo GitHub</a>
+ */
+
+/**
+ * Function qui permet de récupérer les données d'un utilisateur
+ * @type {function(number): Promise<any>}
+ * @param {number} id ID de l'utilisateur
+ * @returns {Promise<any>}
+ */
 
 export async function getUserDatas(id) {
     const userDatas = await fetch(`http://localhost:3000/user/${id}`)
@@ -6,6 +21,13 @@ export async function getUserDatas(id) {
     return userDatas;
 }
 
+/**
+ * Function qui permet de récupérer les performances d'un utilisateur
+ * @type {function(number): Promise<any>}
+ * @param {number} id ID de l'utilisateur
+ * @returns {Promise<any>}
+ */
+
 export async function getUserPerformance(id) {
     const userDatas = await fetch(`http://localhost:3000/user/${id}/performance`)
     .then(response => response.json());
@@ -13,12 +35,26 @@ export async function getUserPerformance(id) {
     return userDatas;
 }
 
+/**
+ * Function qui permet de récupérer les activités d'un utilisateur
+ * @type {function(number): Promise<any>}
+ * @param {number} id ID de l'utilisateur
+ * @returns {Promise<any>}
+ */
+
 export async function getUserActivity(id) {
     const userDatas = await fetch(`http://localhost:3000/user/${id}/activity`)
     .then(response => response.json());
 
     return userDatas;
 }
+
+/**
+ * Function qui permet de récupérer les sessions d'un utilisateur
+ * @type {function(number): Promise<any>}
+ * @param {number} id ID de l'utilisateur
+ * @returns {Promise<any>}
+ */
 
 export async function getUserAverageSessions(id) {
     const userDatas = await fetch(`http://localhost:3000/user/${id}/average-sessions`)
